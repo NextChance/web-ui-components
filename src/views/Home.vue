97ef577285869166aa3c65dc3f8bd79f2586275a
Rafa Garcia <rafael.garcia@therapychat.com>
2019-01-25 12:57:59 +0100

    Created Dumb component to test inside maia

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Dumb/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
+import Dumb from "@/components/Dumb.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    Dumb
   }
 };
 </script>
