65e994a9ca8933f053f0749558658f18552dae93
rafagarcia <rafagarcia@gmail.com>
2019-03-28 17:28:01 +0100

    Update src/components/nc-modal.vue
    PR 2 - Modified css 6
    
    Co-Authored-By: virginiablesa <43244317+virginiablesa@users.noreply.github.com>

--- src/components/nc-modal.vue
+++ src/components/nc-modal.vue
@@ -204,7 +204,7 @@ $break-desktop: 769px;
       position: fixed;
       border-radius: 2px;
       box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
-      -webkit-box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+      -webkit-box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
       -moz-box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
       transition: all .3s ease;
     }
