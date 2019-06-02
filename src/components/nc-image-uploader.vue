a1f804b4b8d5c5173f90865384930ad1bfee1b87
elenamlopez <elena.mateos1@gmail.com>
2019-06-02 11:30:41 +0200

    Eliminates extra coments

--- src/components/nc-image-uploader.vue
+++ src/components/nc-image-uploader.vue
@@ -1,57 +1,56 @@
 <template>
   <div class="image-uploader__container">
     <div class="box">
-					<input 
-            id="imageUploader" 
-            type="file" 
-            name="imageUploader[]" 
-            class="image-uploader_input image-uploader_input-4" 
-            data-multiple-caption="{count} files selected" 
-            multiple=""
-            @change="onFileChange"
-            :disabled="isDisabled"
-            >
-					<label 
-            for="imageUploader"
-            class="nc-image-uploader_background"
-            :style="{ 
-              borderRadius: radius,
-              imageUploaderheight: imageUploaderheight,
-              imageUploaderwidth: imageUploaderwidth,
-              backgroundImage: `url(${bgImage})`
-            }"
-          >
-              <figure v-if="isEmpty"
+      <input 
+        id="imageUploader" 
+        type="file" 
+        name="imageUploader[]" 
+        class="image-uploader_input image-uploader_input-4" 
+        data-multiple-caption="{count} files selected" 
+        multiple=""
+        @change="onFileChange"
+        :disabled="isDisabled"
+        >
+      <label 
+        for="imageUploader"
+        class="nc-image-uploader_background"
+        :style="{ 
+          borderRadius: radius,
+          imageUploaderheight: imageUploaderheight,
+          imageUploaderwidth: imageUploaderwidth,
+          backgroundImage: `url(${bgImage})`
+        }"
+        >
+            <figure v-if="isEmpty"
+            :style="{
+            height: iconHeight,
+            width: iconWidth,
+              }">
+              <slot name="isEmptyState">
+                <!-- icon for empty input -->
+              </slot>  
+            </figure>
+            <figure v-if="isLoading"
               :style="{
-              height: iconHeight,
-              width: iconWidth,
-                }">
-                <slot name="isEmptyState">
-                  <!-- icon for empty input -->
-                </slot>  
-              </figure>
-              <figure v-if="isLoading"
-                :style="{
-                  transform: loaderIconScale,
-                  height: loaderIconHeigth,
-                  width: loaderIconWidth
-                }">
-                <slot name="isLoadingState">
-                  <!-- icon for empty input -->
-                </slot>  
-              </figure>
-              <figure 
-                v-if="withData"
-                @click="removeImage">
-                <slot 
-                  name="withDataState" 
-                  >
-                  <!-- icon for empty input -->
-                </slot>  
-              </figure>
-              <!-- <svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17"><path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"></path></svg> -->
-          </label>
-				</div>
+                transform: loaderIconScale,
+                height: loaderIconHeigth,
+                width: loaderIconWidth
+              }">
+              <slot name="isLoadingState">
+                <!-- icon for loading -->
+              </slot>  
+            </figure>
+            <figure 
+              v-if="withData"
+              @click="removeImage">
+              <slot 
+                name="withDataState" 
+                >
+                <!-- icon for with a background-image -->
+              </slot>  
+            </figure>
+        </label>
+      </div>
     <div 
       class="image-uploader__error" 
       v-if="error"
