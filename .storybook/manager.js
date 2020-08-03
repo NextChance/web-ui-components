97b1e299ef6329761f847bc29c24cce644a7b4fe
Hugo Hernandez <hugo.hernandez@nc43tech.com>
2020-08-03 13:09:56 +0100

    feature(): Add storybook configuration

---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons';
+import nc43theme from './theme';
+
+addons.setConfig({
+    theme: nc43theme,
+});
---
+++ src/components/nc-button/nc-button.stories.js
@@ -0,0 +1,14 @@
+import { action } from '@storybook/addon-actions';
+
+import NcButton from './nc-button';
+
+export default {
+    title: 'NC-BUTTON',
+    component: NcButton,
+};
+
+export const regular = () => ({
+    components: { NcButton },
+    template: '<div class="nc-button-container"> <nc-button @button-clicked="action">Hello Button</nc-button> </div> ',
+    methods: { action: action('clicked') },
+});
